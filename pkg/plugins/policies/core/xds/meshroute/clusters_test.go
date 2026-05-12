21f0678961060bcf9a4aef45182d292e6e98fbf7
Marcin Skalski <skalskimarcin33@gmail.com>
2026-05-12 11:01:50 +0200

    fix(review): address PR review comments
    Signed-off-by: Marcin Skalski <skalskimarcin33@gmail.com>

--- pkg/plugins/policies/core/xds/meshroute/clusters_test.go
+++ pkg/plugins/policies/core/xds/meshroute/clusters_test.go
@@ -7,9 +7,11 @@ import (
 	"github.com/kumahq/kuma/v2/pkg/core/kri"
 	core_meta "github.com/kumahq/kuma/v2/pkg/core/metadata"
 	meshservice_api "github.com/kumahq/kuma/v2/pkg/core/resources/apis/meshservice/api/v1alpha1"
+	core_model "github.com/kumahq/kuma/v2/pkg/core/resources/model"
 	"github.com/kumahq/kuma/v2/pkg/plugins/policies/core/rules/resolve"
 	"github.com/kumahq/kuma/v2/pkg/plugins/policies/core/xds/meshroute"
 	"github.com/kumahq/kuma/v2/pkg/test/resources/builders"
+	xds_context "github.com/kumahq/kuma/v2/pkg/xds/context"
 )
 
 var _ = Describe("SniForBackendRef", func() {
@@ -53,5 +55,65 @@ var _ = Describe("SniForBackendRef", func() {
 		sni := meshroute.SniForBackendRef(ref, ms, port, "kuma-system")
 
 		Expect(sni).To(ContainSubstring(ms.SNIName("kuma-system")))
+	})
+})
+
+var _ = Describe("DestinationPortFromRef", func() {
+	newMeshCtx := func(resources ...core_model.Resource) xds_context.MeshContext {
+		return xds_context.MeshContext{
+			BaseMeshContext: &xds_context.BaseMeshContext{
+				DestinationIndex: xds_context.NewDestinationIndex(resources),
+			},
+		}
+	}
+
+	It("returns false when SectionName does not resolve to a port", func() {
+		ms := builders.MeshService().
+			WithName("backend").
+			WithMesh("default").
+			AddIntPortWithName(8080, 8080, core_meta.ProtocolHTTP, "http").
+			Build()
+
+		id := kri.WithSectionName(kri.From(ms), "unknown")
+		ref := &resolve.RealResourceBackendRef{Resource: id}
+
+		dest, port, ok := meshroute.DestinationPortFromRef(newMeshCtx(ms), ref)
+
+		Expect(ok).To(BeFalse())
+		Expect(dest).To(BeNil())
+		Expect(port).To(BeNil())
+	})
+
+	It("returns false when destination service is missing", func() {
+		id := kri.Identifier{
+			ResourceType: meshservice_api.MeshServiceType,
+			Mesh:         "default",
+			Name:         "missing",
+			SectionName:  "http",
+		}
+		ref := &resolve.RealResourceBackendRef{Resource: id}
+
+		dest, port, ok := meshroute.DestinationPortFromRef(newMeshCtx(), ref)
+
+		Expect(ok).To(BeFalse())
+		Expect(dest).To(BeNil())
+		Expect(port).To(BeNil())
+	})
+
+	It("returns resolved destination and port when SectionName matches", func() {
+		ms := builders.MeshService().
+			WithName("backend").
+			WithMesh("default").
+			AddIntPortWithName(8080, 8080, core_meta.ProtocolHTTP, "http").
+			Build()
+
+		id := kri.WithSectionName(kri.From(ms), "http")
+		ref := &resolve.RealResourceBackendRef{Resource: id}
+
+		dest, port, ok := meshroute.DestinationPortFromRef(newMeshCtx(ms), ref)
+
+		Expect(ok).To(BeTrue())
+		Expect(dest).NotTo(BeNil())
+		Expect(port.GetValue()).To(BeEquivalentTo(8080))
 	})
 })
